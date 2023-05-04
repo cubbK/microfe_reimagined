57115bec467ca0e454d5aaa51cd6ecb234d36dec
Dan Popa <dan.popa@hm.com>
2023-05-04 19:06:36 +0200

    add svelte micro

---
+++ packages/svelte-microfe/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    let count = 0
+    const increment = () => {
+      count += 1
+    }
+  </script>
+  
+  <button on:click={increment} class="hello">
+    count is {count} from svelte
+  </button>
+  
+  <style>
+    .hello {
+      font-size: 2rem;
+      font-weight: bold;
+      color: #222;
+    }
+  </style>
+  
