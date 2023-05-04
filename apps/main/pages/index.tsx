61f566174c3911736a57d9272d559d147c823f39
Dan Popa <dan.popa@hm.com>
2023-05-04 20:09:43 +0200

    render svelte once?

--- apps/main/pages/index.tsx
+++ apps/main/pages/index.tsx
@@ -1,10 +1,10 @@
 import Header from '@micro/header-microfe'
 import Main from '@micro/main-microfe'
-import * as styles from './index.styles'
+import styles from '../helpers/index.styles.module.css'
 import { reactify } from 'svelte-preprocess-react'
-// import AppSvelteRaw from '@micro/svelte-microfe/src/App.svelte'
+import AppSvelteRaw from '@micro/svelte-microfe/src/App.svelte'
 
-// const AppSvelte = reactify(AppSvelteRaw)
+const AppSvelte = reactify(AppSvelteRaw)
 
 export default function Index() {
   return (
@@ -14,6 +14,9 @@ export default function Index() {
       </div>
       <div className={styles.main}>
         <Main />
+      </div>
+      <div className={styles.notReact}>
+        <AppSvelte />
       </div>
     </div>
   )
